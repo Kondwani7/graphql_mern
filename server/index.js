884ff2846436e03287d91c984b6b4b5c30b12440
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-30 12:45:42 +0200

    connected to mongodb database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+const colors = require('colors')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
 const port = process.env.PORT || 5000;
-
+const connectDB = require('./config/db')
 const app = express()
-
+//connect to db
+connectDB()
+//connect to server
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
