64db38838b542881f5e97016a39e3a9e203e813b
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-30 13:06:37 +0200

    graphql server requests routed to our mongodb now

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,9 @@
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql')
-const {projects, clients} = require('../sampleData')
+//const {projects, clients} = require('../sampleData')
+
+const Project = require('../models/Project')
+const Client = require('../models/Client')
+
 //clients
 const ClientType = new GraphQLObjectType({
     name: 'Client',
@@ -22,7 +26,7 @@ const ProjectType = new GraphQLObjectType({
         client: {
             type: ClientType,
             resolve(parent, args){
-                return clients.find(client => client.id === parent.clientId);
+                return Client.findById(parent.clientId)
             }
         }
     })
@@ -34,27 +38,27 @@ const RootQuery = new GraphQLObjectType({
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args){
-                return clients
+                return Client.find()
             }
         },
         client: {
             type: ClientType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id)
+                return Client.findById(args.id)
             }
         },
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args){
-                return projects
+                return Project.find()
             }
         },
         project: {
             type: ProjectType,
             args : {id: {type: GraphQLID}},
             resolve(parent, args){
-                return projects.find(project => project.id == args.id)
+                return Project.findById(args.id)
             }
         }
     }
